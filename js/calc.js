7b72b52926003da8de1974e3f4665345cda003dd
JDesign8 <115661413+JDesign8@users.noreply.github.com>
2022-12-12 14:14:00 -0500

    Initial commit

---
+++ js/calc.js
@@ -0,0 +1,21 @@
+let result = document.getElementById('result');
+
+function addNum(){
+let firstNum = 2;
+let secondNum = 2;
+result.innerHTML = firstNum + secondNum;
+}
+
+function funcName(){
+let   oneNum = 10;
+let twoNum = 10;
+result.innerHTML = oneNum + twoNum;
+}
+
+let calculate = document.elementFromPoint('total');
+
+function totalNum(){
+let oNum = 2; 
+let tNum = 4;
+calculate.innerHTML = oNum + tNum;
+}
